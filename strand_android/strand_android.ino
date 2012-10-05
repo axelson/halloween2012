7664f8f6a1bb4c7d65209d7e9e271ff45c635e3f
Jason Axelson <jason.axelson@gmail.com>
2012-10-05 13:53:04 -1000

    whitespace fixes

--- strand_android/strand_android.ino
+++ strand_android/strand_android.ino
@@ -7,8 +7,8 @@
 
 // Choose which 2 pins you will use for output.
 // Can be any valid output pins.
-int dataPin = 2;   
-int clockPin = 3; 
+int dataPin = 2;
+int clockPin = 3;
 
 // Set the first variable to the NUMBER of pixels. 32 = 32 pixels in a row
 // The LED strips are 32 LEDs per meter but you can extend/cut the strip
@@ -17,19 +17,20 @@ LPD8806 strip = LPD8806(32, dataPin, clockPin);
 // you can also use hardware SPI, for ultra fast writes by leaving out the
 // data and clock pin arguments. This will 'fix' the pins to the following:
 // on Arduino 168/328 thats data = 11, and clock = pin 13
-// on Megas thats data = 51, and clock = 52 
+// on Megas thats data = 51, and clock = 52
 //LPD8806 strip = LPD8806(32);
 
 static int counter = 0;
 static int sleepTime = 2000;
 static int niceColors = 0;
+
 void setup() {
   // Start up the LED strip
   strip.begin();
 
   // Update the strip, to start they are all 'off'
   strip.show();
-  
+
   Serial.begin(115200);
 }
 
@@ -84,27 +85,27 @@ void loop() {
     colorWipe(strip.Color(0,0,127), 10);		// blue
   }
 //  rainbow(10);
-  
+
   //rainbowCycle(0);  // make it go through the cycle fairly fast
 }
 
 void rainbow(uint8_t wait) {
   int i, j;
-   
+
   for (j=0; j < 384; j++) {     // 3 cycles of all 384 colors in the wheel
     for (i=0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel( (i + j) % 384));
-    }  
+    }
     strip.show();   // write all the pixels out
     delay(wait);
   }
 }
 
-// Slightly different, this one makes the rainbow wheel equally distributed 
+// Slightly different, this one makes the rainbow wheel equally distributed
 // along the chain
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
-  
+
   for (j=0; j < 384 * 5; j++) {     // 5 cycles of all 384 colors in the wheel
     for (i=0; i < strip.numPixels(); i++) {
       // tricky math! we use each pixel as a fraction of the full 384-color wheel
@@ -112,7 +113,7 @@ void rainbowCycle(uint8_t wait) {
       // Then add in j which makes the colors go around per pixel
       // the % 384 is to make the wheel cycle around
       strip.setPixelColor(i, Wheel( ((i * 384 / strip.numPixels()) + j) % 384) );
-    }  
+    }
     strip.show();   // write all the pixels out
     delay(wait);
   }
@@ -122,7 +123,7 @@ void rainbowCycle(uint8_t wait) {
 // good for testing purposes
 void colorWipe(uint32_t c, uint8_t wait) {
   int i;
-  
+
   for (i=0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, c);
       strip.show();
@@ -134,14 +135,14 @@ void colorWipe(uint32_t c, uint8_t wait) {
 // good for testing purposes
 void colorChase(uint32_t c, uint8_t wait) {
   int i;
-  
+
   for (i=0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, 0);  // turn all pixels off
-  } 
-  
+  }
+
   for (i=0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, c);
-      if (i == 0) { 
+      if (i == 0) {
         strip.setPixelColor(strip.numPixels()-1, 0);
       } else {
         strip.setPixelColor(i-1, 0);
@@ -165,17 +166,17 @@ uint32_t Wheel(uint16_t WheelPos)
       r = 127 - WheelPos % 128;   //Red down
       g = WheelPos % 128;      // Green up
       b = 0;                  //blue off
-      break; 
+      break;
     case 1:
       g = 127 - WheelPos % 128;  //green down
       b = WheelPos % 128;      //blue up
       r = 0;                  //red off
-      break; 
+      break;
     case 2:
-      b = 127 - WheelPos % 128;  //blue down 
+      b = 127 - WheelPos % 128;  //blue down
       r = WheelPos % 128;      //red up
       g = 0;                  //green off
-      break; 
+      break;
   }
   return(strip.Color(r,g,b));
 }
